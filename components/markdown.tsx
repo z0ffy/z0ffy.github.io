899f7d4b8482b539f79482ce0a56eef999e585b1
z0ffy <654109707@qq.com>
2024-10-23 00:02:04 +0800

    feat: 2024

--- components/markdown.tsx
+++ components/markdown.tsx
@@ -1,7 +1,6 @@
 import {useTheme} from 'next-themes'
 import {useEffect} from 'react'
-// @ts-ignore
-import {ReactMarkdown} from 'react-markdown/lib/react-markdown'
+import ReactMarkdown from 'react-markdown'
 import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter'
 import {a11yDark as dark, prism as light} from 'react-syntax-highlighter/dist/cjs/styles/prism'
 import rehypeRaw from 'rehype-raw'
