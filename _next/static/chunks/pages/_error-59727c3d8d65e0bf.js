a76a50c308ed3104999b2bee2f2ba0b91923c64b
z0ffy <z0ffy@users.noreply.github.com>
2025-01-09 08:20:20 +0000

    deploy: 4554f144cae64d93229ac44c8c9e94784244d676

---
+++ _next/static/chunks/pages/_error-59727c3d8d65e0bf.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[731],{29882:(_,n,e)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return e(79853)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[636,593,792],()=>n(29882)),_N_E=_.O()}]);
